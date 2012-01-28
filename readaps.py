1e48755b45d7618773f904c8867391790ff091c7
Thomas Georgiou <tageorgiou@gmail.com>
2012-01-28 14:58:30 -0500

    Add in coordinates and lecture hall

--- readaps.py
+++ readaps.py
@@ -1,5 +1,6 @@
 import json
 buildingdb = json.load(open("buildingcoords.json"))
+roomsdb = json.load(open("rooms.json"))
 aplist = filter(lambda l: len(l) == 2, map(lambda l: l.split(","),
     open("aps.csv").read().split('\n')[1::]))
 fout = open("aplist.csv","w")
@@ -7,11 +8,17 @@ fout.write("bssid, building, floor, lat, lon\n")
 for aplocation, bssid in aplist:
     try:
         building = aplocation.split("-")[0].upper()
+        room = aplocation.split("-")[1].upper()
         if building[0] == 'M':
             building = building[1::]
         floor = aplocation.split("-")[1][0]
-        lat = buildingdb[building]['lat']
-        lon = buildingdb[building]['lon']
+        fullroom = building + "-" + room
+        if fullroom in roomsdb.keys():
+            lat = float(roomsdb[fullroom]['lat']) / 1e6
+            lon = float(roomsdb[fullroom]['lon']) / 1e6
+        else:
+            lat = buildingdb[building]['lat']
+            lon = buildingdb[building]['lon']
         fout.write("%s,%s,%s,%s,%s\n" % (bssid, building, floor, lat, lon))
     except Exception as e:
         print e
